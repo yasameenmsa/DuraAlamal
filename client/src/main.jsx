73a5b315f3d8815d4eca05a96ba5733af85bac08
arrealstate <131165262+yasameenmsa@users.noreply.github.com>
2024-07-05 03:35:29 +0300

    About ٌReady

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,14 +6,17 @@ import { store, persistor } from './redux/store.js';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import ThemeProvider from './components/ThemeProvider.jsx';
-import './i18n'; 
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <PersistGate persistor={persistor}>
-    <Provider store={store}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </Provider>
-  </PersistGate>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <I18nextProvider i18n={i18n}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </I18nextProvider>
+    </PersistGate>
+  </Provider>
 );
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,27 +2,26 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 function About() {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <div className='min-h-screen flex items-center justify-center'>
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            {useTranslation('aboutTitle')}
-            {/* {t('aboutTitle')} */}
+            {t('aboutTitle')}
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
-              {/* {t('aboutContent1')} */}
+              {t('aboutContent1')}
             </p>
 
             <p>
-              {/* {t('aboutContent2')} */}
+              {t('aboutContent2')}
             </p>
 
             <p>
-              {/* {t('aboutContent3')} */}
+              {t('aboutContent3')}
             </p>
           </div>
         </div>
